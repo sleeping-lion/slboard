aa6ff3a39e22116087752146b08c3853577f1949
sleeping-lion <toughjjh@gmail.com>
2016-01-26 22:10:27 +0900

    update

--- db/migrate/20120516002154_create_devise_to_users.rb
+++ db/migrate/20120516002154_create_devise_to_users.rb
@@ -1,24 +1,16 @@
 # encoding: utf-8
-
 class CreateDeviseToUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.references :group, :null=>false      
+      t.references :group, :null=>false
       ## Database authenticatable
-      t.string :email,  :null => false      
+      t.string :email,  :null => false
       t.string :name, :null => false, :limit=>60
       t.string :nickname, :null => false, :limit=>60
       t.string :photo, :null=>false
       t.string :encrypted_password, :null => false
-      t.string :description      
-      t.string :alternate_name, :limit=>60
-      t.boolean :gender, :default=>true      
-      t.date :birth_date
-      t.date :death_date
-      t.string :url
-      t.string :job_title, :limit=>60
+      t.string :description
 
-      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -43,24 +35,21 @@ class CreateDeviseToUsers < ActiveRecord::Migration
       # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-      
-      t.integer :user_photos_count,:default=>0,:null=>false      
 
       ## Token authenticatable
       # t.string :authentication_token
       t.boolean :admin, :null=>false, :default => false
-      t.boolean :intro, :null=>false, :default=>false
       t.boolean :enable, :null=>false, :default=>true
       t.timestamps :null=>false
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps
+    # Uncomment below if timestamps were not included in your original model.
+    # t.timestamps
     end
-    
+
     add_index :users, :group_id
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
-    # add_index :users, :authentication_token, :unique => true        
+  # add_index :users, :confirmation_token,   :unique => true
+  # add_index :users, :unlock_token,         :unique => true
+  # add_index :users, :authentication_token, :unique => true
   end
 end
